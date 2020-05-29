2468d16bb9f92e01db3fb8d81c176f5b6e407257
plusbeauxjours <plusbeauxjours@gmail.com>
2020-05-29 14:30:56 +0700

    heroku

--- config/settings.py
+++ config/settings.py
@@ -46,6 +46,8 @@ else:
     DATABASES = {
         "default": env.db("DATABASE_URL"),
     }
+    db_from_env = dj_database_url.config(conn_max_age=500)
+    DATABASES["default"].update(db_from_env)
 
 
 ALLOWED_HOSTS = ["*"]
@@ -86,6 +88,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = "config.urls"
