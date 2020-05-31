f941b22c4ce78af3324dcb2bcac7e46893b16d41
plusbeauxjours <plusbeauxjours@gmail.com>
2020-06-01 00:50:15 +0700

    commit to heroku

--- config/settings.py
+++ config/settings.py
@@ -28,7 +28,7 @@ ROOT_DIR = environ.Path(__file__) - 2
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 
 # Database
