1ecbe081f85bee07fc6c64c591cda57463625263
plusbeauxjours <plusbeauxjours@gmail.com>
2020-06-30 20:18:47 +0700

    UPDATE action on userAdmin

--- users/admin.py
+++ users/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from . import models
+from classes import models as class_models
 
 
 @admin.register(models.User)
 class UserAdmin(admin.ModelAdmin):
+    actions = ['update_has_paid']
     list_display = (
         "username",
         "class_order",
@@ -31,3 +33,9 @@ class UserAdmin(admin.ModelAdmin):
     search_fields = ('first_name', 'last_name', 'username')
     list_filter = ('class_order', 'has_paid', 'gender',
                    'has_apple_account', 'has_submitted_application')
+
+    def update_has_paid(self, request, queryset):
+        class_order = class_models.ClassOrder.objects.last()
+        queryset.update(has_paid=True, class_order=class_order)
+
+    update_has_paid.short_description = "💳결제"
