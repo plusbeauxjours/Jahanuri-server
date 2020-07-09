456829b5aac31db76eca80665df1e50da6f0a132
plusbeauxjours <plusbeauxjours@gmail.com>
2020-07-10 01:31:23 +0700

    DEPLOY

--- feeds/models.py
+++ feeds/models.py
@@ -6,12 +6,12 @@ from core import models as core_models
 
 class Feed(core_models.TimeStampedModel):
     class_order = models.ForeignKey(
-        "classes.ClassOrder", on_delete=models.PROTECT, blank=True, null=True,  verbose_name="기수"
+        "classes.ClassOrder", on_delete=models.CASCADE, blank=True, null=True,  verbose_name="기수"
     )
     uuid = models.UUIDField(
         default=uuid.uuid4, editable=False, unique=True, verbose_name="고유 번호")
     user = models.ForeignKey(
-        "users.User", on_delete=models.PROTECT, verbose_name="회원")
+        "users.User", on_delete=models.CASCADE, verbose_name="회원")
     text = models.TextField(blank=True, null=True, verbose_name="공지")
 
     def __str__(self):
